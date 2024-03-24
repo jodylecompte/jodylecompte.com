6281942e89ddf192a6ea881c9c23096c021dec51
Jody LeCompte <jlecompte@celebrationtravelgroup.com>
2024-03-24 11:20:27 -0500

    Add google analyrtics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
-
+import partytown from '@astrojs/partytown'
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+import { defineConfig } from 'astro/config';
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+  	partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),]
 });
