a05e6fcbd5ca9b22b305ea54bf9692f4d852ca13
Jody LeCompte <jody@jodylecompte.com>
2024-05-27 12:44:25 -0500

    First new post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -279,8 +279,8 @@ export default async function Home() {
             ))}
           </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
-            <Newsletter />
-            <Resume />
+            {/*<Newsletter />*/}
+            {/*<Resume />*/}
           </div>
         </div>
       </Container>
