9bb210f264b086de12537d8fc90a24c18023e5cc
Jody LeCompte <jody@jodylecompte.com>
2024-05-27 13:37:22 -0500

    Add redirect for updated blog route

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,20 @@
-import rehypePrism from '@mapbox/rehype-prism'
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism';
+import nextMDX from '@next/mdx';
+import remarkGfm from 'remark-gfm';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-}
+  redirects: async () => {
+    return [
+      {
+        source: '/posts/your-audience-wants-you-to-win',
+        destination: '/articles/your-audience-wants-you-to-win',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
@@ -13,6 +22,6 @@ const withMDX = nextMDX({
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypePrism],
   },
-})
+});
 
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
