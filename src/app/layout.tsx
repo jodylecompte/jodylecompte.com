a64ef9a8b6d1f7c4d88eddd68ff88041f165f6d5
Jody LeCompte <jody@jodylecompte.com>
2024-05-27 13:59:19 -0500

    Fix missed meta desc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
       'Jody LeCompte - Senior Full-Stack Developer specializing in TypeScript.',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    ' I\'m Jody, a seasoned full stack software developer with over a decade of experience specializing in TypeScript, Node, React, and Angular.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
