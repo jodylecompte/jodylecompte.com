e7315f599d82cd7f8625fa813d2003b1262e9cd8
Jody LeCompte <jody@jodylecompte.com>
2024-07-27 10:45:36 -0500

    Remove stock astronaut picture

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -56,16 +56,7 @@ export default function About() {
   return (
     <Container className="mt-16 sm:mt-32">
       <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
-        <div className="lg:pl-20">
-          <div className="max-w-xs px-2.5 lg:max-w-none">
-            <Image
-              src={portraitImage}
-              alt=""
-              sizes="(min-width: 1024px) 32rem, 20rem"
-              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
-            />
-          </div>
-        </div>
+     
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
             I’m Jody LeCompte, an unapologetically nerdy software developer from South Louisiana.
