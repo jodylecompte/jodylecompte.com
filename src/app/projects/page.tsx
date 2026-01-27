f4e92fd544dd2d99e9cb6b78ab67dfd3673b9d1f
Jody LeCompte <jody@jodylecompte.com>
2026-01-27 07:02:33 -0600

    Add greek alphabet trainer to projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,6 +5,18 @@ import { SimpleLayout } from '@/components/SimpleLayout'
 
 const projects = [
   {
+    name: 'Greek Alphabet Trainer',
+    description:
+      'A one-year reading plan and plan progress tracking app for reading the entire English Bible and all major reformation creeds, confessons, and catechisms.',
+    links: [
+      { href: 'https://greekalphabettrainer.com', label: 'Live' },
+      {
+        href: 'https://github.com/jodylecompte/greekalphabettrainer.com',
+        label: 'GitHub',
+      },
+    ],
+  },
+  {
     name: 'Solas Bible Plan',
     description:
       'A one-year reading plan and plan progress tracking app for reading the entire English Bible and all major reformation creeds, confessons, and catechisms.',
