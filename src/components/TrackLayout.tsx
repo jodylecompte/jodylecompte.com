9ca4bf8f20316c7d570cee1e288afefd5a8d32b6
Jody LeCompte <jody@jodylecompte.com>
2026-02-19 10:54:22 -0600

    Publish leraing AI in public track

--- src/components/TrackLayout.tsx
+++ src/components/TrackLayout.tsx
@@ -1,10 +1,7 @@
-import Link from 'next/link'
-
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
 import { Prose } from '@/components/Prose'
-import { type ArticleWithSlug } from '@/lib/articles'
-import { getArticlesByTrack } from '@/lib/articles'
+import { type ArticleWithSlug, getArticlesByTrack } from '@/lib/articles'
 import { type TrackWithSlug } from '@/lib/tracks'
 import { formatDate } from '@/lib/formatDate'
 
@@ -48,31 +45,33 @@ export async function TrackLayout({
 
   return (
     <Container className="mt-16 sm:mt-32">
-      <header className="max-w-2xl">
-        <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-          {track.title}
-        </h1>
-        <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-          {track.description}
-        </p>
-      </header>
-      <div className="mt-16 sm:mt-20">
-        <Prose data-mdx-content>{children}</Prose>
-        {articles.length > 0 && (
-          <div className="mt-16">
-            <h2 className="text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100">
-              Posts in this track
-            </h2>
-            <div className="mt-8 md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-              <div className="flex max-w-3xl flex-col space-y-16">
-                {articles.map((article) => (
-                  <TrackArticle key={article.slug} article={article} />
-                ))}
-              </div>
+      <div className="mx-auto max-w-2xl">
+        <header>
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+            {track.title}
+          </h1>
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+            {track.description}
+          </p>
+        </header>
+        <Prose className="mt-8" data-mdx-content>
+          {children}
+        </Prose>
+      </div>
+      {articles.length > 0 && (
+        <div className="mt-16 sm:mt-20">
+          <h2 className="max-w-2xl text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100">
+            Posts in this track
+          </h2>
+          <div className="mt-8 md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+            <div className="flex max-w-3xl flex-col space-y-16">
+              {articles.map((article) => (
+                <TrackArticle key={article.slug} article={article} />
+              ))}
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </Container>
   )
 }
